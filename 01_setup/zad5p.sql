aa2dbe9cf637bbcb4e666323deb1df5ccd437ba2
witoldtomczyk <148116416+witoldtomczyk@users.noreply.github.com>
2025-11-03 11:12:17 +0100

    Update zad5p.sql

--- 01_setup/zad5p.sql
+++ 01_setup/zad5p.sql
@@ -1,1 +1,8 @@
+DELETE FROM pracownicy
+WHERE id_pracownika IN (
+    SELECT w.id_pracownika
+    FROM wynagrodzenie w
+    JOIN pensja p ON w.id_pensji = p.id_pensji
+    WHERE p.kwota < 1200
+);
 
