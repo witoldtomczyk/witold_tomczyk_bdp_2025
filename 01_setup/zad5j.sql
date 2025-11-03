4bace81d989eefba7d93b43438014fd529779b67
witoldtomczyk <148116416+witoldtomczyk@users.noreply.github.com>
2025-11-03 11:10:53 +0100

    Update zad5j.sql

--- 01_setup/zad5j.sql
+++ 01_setup/zad5j.sql
@@ -1,1 +1,5 @@
-
+SELECT imie, nazwisko, p.kwota, pr.kwota FROM pracownicy prac
+JOIN wynagrodzenie w ON prac.id_pracownika=w.id_pracownika
+JOIN pensja p ON w.id_pensji=p.id_pensji
+JOIN premia pr ON pr.id_premii=w.id_premii
+ORDER BY p.kwota DESC, pr.kwota DESC;
